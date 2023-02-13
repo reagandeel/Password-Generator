5e1ce5e9f09c6e6c35db1c2b9a6a6b9ad10f8327
reagandeel <reagandeel1821@gmail.com>
2023-02-12 19:27:17 -0600

    tested out adding a function

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); //generate password button
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword(); // calling generate password function and storing value into 'password' var
@@ -9,19 +10,14 @@ function writePassword() {
   passwordText.value = password; // takes value of generated password & plugs it into the screen
 
 }
+function generatePassword() {
+  console.log ("test")
+}
 
-generateBtn.addEventListener("click".writePassword); // when user clicks the generate button, it will call the password function.
+generateBtn.addEventListener("click",writePassword); // when user clicks the generate button, it will call the password function.
 
 
 /*
-function generateBtn {
-    document.getElementbyId = generatePassword();
-
-}
-
-  document.writePassword("password").value {
-
-
 }
   for (var i = 0; i <= passwordLength; i++) {
     var randomNumber = Math.floor(Math.random("#generate") * chars.length);
