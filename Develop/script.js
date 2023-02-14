9ddeea90cbb4e6e372a0cdd56abb15e0d56d65ea
reagandeel <reagandeel1821@gmail.com>
2023-02-13 18:23:50 -0600

    assigned functions to different arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,15 @@
 // Assignment Code
+
+// 'Generate Password' button
 var generateBtn = document.querySelector("#generate"); //generate password button
-var SpecialCharArray = ['!','@','#','$','*','&','[', ']', '(', ')','%', '>', '<', '+', '.', ':', ';','=','^','{', '}', ',','_' ]; // !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
-var uppercaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var lowercaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var numArray = ['0','1','2','3','4','5','6','7','8','9']
+
+// Setting specifications for the password
+var specialCharArray = ['!','@','#','$','*','&','[', ']', '(', ')','%', '>', '<', '+', '.', ':', ';','=','^','{', '}', ',','_' ]; // !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+var upperCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCaseArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numArray = ['0','1','2','3','4','5','6','7','8','9'];
+var emptyArray = [];
+
 
 // Write password to the #password input
 function writePassword() {
@@ -19,9 +25,30 @@ length: 8-128 char
 lowercase, uppercase, numeric, and/or special characters
 once answered > input validated, one char type selected, password with correct criteria generated & displayed
 */
+
+
 function generatePassword() {
   console.log ("test")
   var password = '';
+
+  //Prompt for uppercase letters
+ function masterCharArray () {
+  var promptUppercase = window.prompt ('Would you like to include Uppercase letters? Type "YES" or "NO"')
+    if (promptUppercase === "YES") {
+      window.confirm ('Your password will include uppercase letters.')
+     } else {
+        masterCharArray = emptyArray;
+      }
+    }
+  //Prompt for lowercase letters
+  
+  var promptLowercase = window.prompt ('Would you like to include lowercase letters? Type "YES" or "NO"')
+    if (promptLowercase === "YES") {
+      window.confirm ('Your password will include lowercase letters.')
+    } else {
+        password= emptyArray;
+      }
+    }
   var length = 8,
         charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
         retVal = "";
